78255a6ca258126a14493f60b61c2eb398373125
Hubot <leo.chris@gmail.com>
2018-09-21 13:46:16 -0400

    ye

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -1,4 +1,17 @@
+import os
 print("****SlopeCalculator****")
+bool = 1
+n = 0
+
+q = input("Would you like sound")
+if q = "yes" {
+	n = 1
+}
+else if q = "no" {
+	n = 0
+}
+
+os.system("say Hello, Welcome to the slope calculator")
 
 x1 = input("Input x1: ")
 y1 = input("Input y1: ")
@@ -11,7 +24,7 @@ print("Your 2nd point is (%s" % x2 + " , %s" % y2 + ")")
 
 rise = x2-x1
 run = y2-y1
-slope = float(x2 - x1)/(y2-y1)
+slope = float(y2-y1)/(x2-x1)
 
 print("Your rise is %s" % rise)
 print("Your run is %s" % run)
