3e69bd99ed5975240f7cf60443b56a03ec334178
Hubot <leo.chris@gmail.com>
2018-09-25 08:33:59 -0400

    yeap

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -2,19 +2,17 @@ import os
 print("****SlopeCalculator****")
 
 bool = 1
-n = 0
 yes = "yes"
 no = "no"
 
-q = input("Would you like sound")
-if q in ['yes']:
-    n = 1
+q = input("Would you like sound: ")
+if q.lower() == "yes":
+    bool = 1
 
 if q in ['no']:
-	n = 0
+	bool = 0
 
 
-bool = n	
 if bool == 1: 
 	os.system("say Hello, Welcome to the slope calculator. You have chosen to have sound Accessibility")
 
@@ -40,7 +38,6 @@ print("Your 2nd point is (%s" % x2 + " , %s" % y2 + ")")
 rise = x2-x1
 run = y2-y1
 slope = float(y2-y1)/(x2-x1)
-sound = string(slope)
 
 print("Your rise is %s" % rise)
 print("Your run is %s" % run)
